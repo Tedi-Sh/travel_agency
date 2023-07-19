670cae7f16a9c174013f4df2d981fcd0f0b47995
User <tedishehu92@gmail.com>
2023-07-19 11:22:35 +0200

    populated db

--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,5 @@
-from django.db.models import IntegerField, CharField, ForeignKey, RESTRICT, TextField, Model, DO_NOTHING
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.db.models import IntegerField, CharField, ForeignKey, RESTRICT, TextField, Model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Country(Model):
@@ -11,7 +11,7 @@ class Country(Model):
 
 class City(Model):
     name = CharField(max_length=50)
-    belong_to_country = ForeignKey(Country, on_delete=DO_NOTHING)
+    belong_to_country = ForeignKey(Country, on_delete=RESTRICT)
 
     def __str__(self):
         return f"{self.name}"
@@ -19,11 +19,11 @@ class City(Model):
 
 class Hotel(Model):
     name = CharField(max_length=100)
-    stars = IntegerField(validators=[MinLengthValidator(0), MaxLengthValidator(5)])
+    stars = IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
 
     descriptions = TextField(max_length=255)
-    belong_to_city = ForeignKey(City, on_delete=DO_NOTHING)
-    price = IntegerField(validators=[MinLengthValidator(1)])
+    belong_to_city = ForeignKey(City, on_delete=RESTRICT)
+    price = IntegerField(validators=[MinValueValidator(1)])
 
     def __str__(self):
         return f"{self.name}"
@@ -31,13 +31,13 @@ class Hotel(Model):
 
 class Airport(Model):
     name = CharField(max_length=100)
-    belong_to_city = ForeignKey(City, on_delete=DO_NOTHING)
-    price = IntegerField(validators=[MinLengthValidator(1)])
+    belong_to_city = ForeignKey(City, on_delete=RESTRICT)
+    price = IntegerField(validators=[MinValueValidator(1)])
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Price(Model):
-    airport_price = ForeignKey(Airport, related_name='airport_price', on_delete=DO_NOTHING)
-    hotel_price = ForeignKey(Hotel, related_name='hotel_price', on_delete=DO_NOTHING)
+    airport_price = ForeignKey(Airport, related_name='airport_price', on_delete=RESTRICT)
+    hotel_price = ForeignKey(Hotel, related_name='hotel_price', on_delete=RESTRICT)
