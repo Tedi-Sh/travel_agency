55ef156c5525b2aa6b046c3ad122501b7965214d
Evelina Xhaja <evigurra@gmail.com>
2023-07-22 11:06:25 +0200

    update

--- viewer/views.py
+++ viewer/views.py
@@ -1,40 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from viewer.models import Hotel, Airport
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from viewer.forms import SignUpForm, SearchForm
+from viewer.models import Hotel
 
 
 def home(request):
-    return HttpResponse("Hello..........")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('home')
+    else:
+        return render(request, 'test_home.html')
 
 
-class SearchForm:
-    pass
+def log_out(request):
+    logout(request)
+    #message.reques("You have logged out")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            #messages.request("Record created)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html',{'form': form})
+    return render(request, 'register.html',{'form': form})
+
 
 
 def search(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
-            from_city = form.cleaned_data['from_city']
-            to_city = form.cleaned_data['to_city']
-            hotel = form.cleaned_data['hotel']
-            departure_date = form.cleaned_data['departure_date']
-            return_date = form.cleaned_data['return_date']
-            trip_type = form.cleaned_data['trip_type']
-            num_adults = form.cleaned_data['num_adults']
-            num_children = form.cleaned_data['num_children']
-
-            # Perform the search
-            # Use the cleaned_data to search for matching trips
-            # This will vary based on your models and database setup
-            # Here is a very basic example, you will need to adjust it to fit your needs
-            hotels = Hotel.objects.filter(city=to_city)
-            airports = Airport.objects.filter(city=from_city)
-
-            # Then pass these results to your template
-            return render(request, 'results.html', {'hotels': hotels, 'airports': airports})
-
+            from_location = form.cleaned_data.get('from_location')
+            to_location = form.cleaned_data.get('to_location')
+            date_of_departure = form.cleaned_data.get('date_of_departure')
+            return_date = form.cleaned_data.get('return_date')
+            number_of_adults = form.cleaned_data.get('number_of_adults')
+            number_of_children = form.cleaned_data.get('number_of_children')
+            trip_type = form.cleaned_data.get('trip_type')  # optional connected to Class Hotel(Model)
+            hotels = Hotel.objects.filter(belong_to_city=to_location)  # , trip_type=trip_type)
+            return render(request, 'search_results.html', {
+                'hotels': hotels
+            })
     else:
         form = SearchForm()
-    return render(request, 'search.html', {'form': form})
-
+    return render(request, 'search_bar.html', {'form': form})
