22c62e865390a4a6d3f1bd2ef581d66876dafcc6
User <tedishehu92@gmail.com>
2023-07-22 09:59:18 +0200

    form and views

--- viewer/forms.py
+++ viewer/forms.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.forms import (
     IntegerField, Form, ChoiceField, ModelChoiceField,
-    Select, DateField, SelectDateWidget, NumberInput
+    Select, DateField, SelectDateWidget, NumberInput, CharField, TextInput
 )
-from viewer.models import City
+from viewer.models import City, Airport, Country
+from django.utils.translation import gettext_lazy as _
 
 
 class SearchForm(Form):
@@ -12,24 +14,44 @@ class SearchForm(Form):
         ('FB', 'Full Board'),
         ('AI', 'All Inclusive')
     ]
-    from_location = ModelChoiceField(queryset=City.objects.all(), widget=Select(attrs={'class': 'form-control'}))
-    to_location = ModelChoiceField(queryset=City.objects.all(), widget=Select(attrs={'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        get_airports = Airport.objects.all()
+        self.fields['from_location'] = ChoiceField(
+            choices=[(airport.id, f'{airport.belong_to_city.name} - {airport.name}')
+                     for airport in get_airports
+                     ],
+            widget=Select(attrs={'class': 'form-control'}),
+            label='From'
+        )
+
+        self.fields['to_location'] = ChoiceField(
+            choices=[(airport.id, f'{airport.belong_to_city.name} - {airport.name}')
+                     for airport in get_airports
+                     ],
+            widget=Select(attrs={'class': 'form-control'}),
+            label='To'
+        )
+
     date_of_departure = DateField(widget=SelectDateWidget(attrs={'class': 'form-control'}))
     return_date = DateField(widget=SelectDateWidget(attrs={'class': 'form-control'}))
-    number_of_adults = IntegerField(min_value=1, widget=NumberInput(attrs={'class': 'form-control'}))
-    number_of_children = IntegerField(min_value=0, widget=NumberInput(attrs={'class': 'form-control'}))
+    number_of_adults = IntegerField(min_value=1, widget=NumberInput(attrs={'class': 'form-control'}),
+                                    label='Nr. of Adults', initial=1)
+    number_of_children = IntegerField(min_value=0, widget=NumberInput(attrs={'class': 'form-control'}),
+                                      label='Nr. of Children', initial=0)
     trip_type = ChoiceField(choices=TRIP_TYPES, widget=Select(attrs={'class': 'form-control'}))
 
-# class SearchForm(Form):
-#     TRIP_TYPE_CHOICES = [
-#         ('BB', 'Bed & Breakfast'),
-#         ('HB', 'Half Board'),
-#         ('FB', 'Full Board'),
-#         ('AI', 'All Inclusive')
-#     ]
-#
-#     from_city = ModelChoiceField(queryset=City.objects.all())
-#     to_city = ModelChoiceField(queryset=City.objects.all())
-#     num_adults = IntegerField(min_value=1)
-#     num_children = IntegerField(min_value=0)
-#     trip_type = ChoiceField(choices=TRIP_TYPE_CHOICES)
+    def clean(self):
+        cleaned_data = super().clean()
+        date_of_departure = cleaned_data.get('date_of_departure')
+        return_date = cleaned_data.get('return_date')
+
+        if date_of_departure and return_date:
+            if return_date < date_of_departure:
+                raise ValidationError({
+                    'return_date': ValidationError(
+                        _('Return date cannot be before departure date.'),
+                        code='invalid'
+                    ),
+                })
--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from viewer.models import Hotel, Airport
+from viewer.models import Hotel, City
 from .forms import SearchForm
 
 
@@ -7,26 +7,19 @@ def search(request):
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
-            # Get data from form
             from_location = form.cleaned_data.get('from_location')
             to_location = form.cleaned_data.get('to_location')
             date_of_departure = form.cleaned_data.get('date_of_departure')
             return_date = form.cleaned_data.get('return_date')
             number_of_adults = form.cleaned_data.get('number_of_adults')
             number_of_children = form.cleaned_data.get('number_of_children')
-            trip_type = form.cleaned_data.get('trip_type')
-
-            # Query hotels and airports based on data
-            hotels = Hotel.objects.filter(belong_to_city=to_location)
-            airports = Airport.objects.filter(belong_to_city__in=[from_location, to_location])
-
-            # Render the results template with queried data
+            trip_type = form.cleaned_data.get('trip_type')  # optional connected to Class Hotel(Model)
+            hotels = Hotel.objects.filter(belong_to_city=to_location)  # , trip_type=trip_type)
             return render(request, 'search_results.html', {
-                'hotels': hotels,
-                'airports': airports,
+                'hotels': hotels
             })
     else:
         form = SearchForm()
+    return render(request, 'search_bar.html', {'form': form})
 
-    return render(request, 'search.html', {'form': form})
 
